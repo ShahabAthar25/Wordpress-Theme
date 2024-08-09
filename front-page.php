4dd04b310af1610b68dcf4200552d26fba879ddf
ShahabAthar25 <shahabathar25@gmail.com>
2024-08-09 13:30:12 +0500

    add bootstrap and connect stylesheet

--- front-page.php
+++ front-page.php
@@ -1,5 +1,14 @@
 <?php get_header() ?>
 
-<h1>Hello World</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <h1>Hello</h1>
+        </div>
+        <div class="col-6">
+            <h1>World</h1>
+        </div>
+    </div>
+</div>
 
 <?php get_footer() ?>
--- functions.php
+++ functions.php
@@ -2,8 +2,9 @@
 
 function load_css()
 {
-    wp_enqueue_style('bootstrap_grid', get_template_directory_uri() . '/css/bootstrap-grid.min.css', array(), false, 'all');
+    wp_enqueue_style('styles', get_template_directory_uri() . '/style.css', array("bootstrap_grid"), true, 'all');
+    wp_enqueue_style('bootstrap_grid', get_template_directory_uri() . '/css/bootstrap-grid.css', array(), true, 'all');
 };
 
 
-add_action('wp_head', 'load_css');
+add_action('wp_enqueue_scripts', 'load_css');
